e23d3a6e71558ae2854b8244f4aa47d1f2820ce1
AgneseAndersone <Agnese.Gabriela.Andersone@gmail.com>
2021-04-17 10:05:08 +0300

    delete folder

--- target/classes/db/migration/V1_0_3__insert_user.sql
+++
@@ -1,6 +0,0 @@
-insert into user values(1, 'user1', 'user1@user.com', '010101-10006', 'Riga, Some Street 111', 'Programming C++');
-insert into user values(2, 'user2', 'user2@user.com', '010102-10007', 'Riga, Other Street 122', 'Naves ena');
-insert into user values(3, 'user3', 'user3@user.com', '010103-10008', 'Riga, First Street 22', 'Poem Strip');
-insert into user values(4, 'user4', 'user4@user.com', '010104-10009', 'Riga, Third Street 52', 'Programming C++');
-insert into user values(5, 'user5', 'user5@user.com', '010105-10010', 'Riga, Fourth Street 82', 'History of Imperator Rome');
-insert into user values(6, 'user6', 'user6@user.com', '010106-10011', 'Riga, Fifth Street 14', 'The Tartar Steppe');
